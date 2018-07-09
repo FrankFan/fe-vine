c1991d7ed09c523096646e0eb174e712bf6818c6
Khan Fan <khan.fan@oriente.com>
2018-07-09 16:59:03 +0800

    add command

--- libs/config.js
+++ libs/config.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const filePath = path.resolve(__dirname, 'oriente.config.json');
+const filePath = path.resolve(__dirname, '../oriente.config.json');
 const json = fs.readFileSync(filePath);
 const config = JSON.parse(json);
 
--- libs/index.js
+++ libs/index.js
@@ -1,7 +1,8 @@
 import exeq from 'exeq';
-import config from '../config';
+import chalk from 'chalk';
+import config from './config';
 
-const upload = () => {
+const upload = (platform = 'android') => {
   const {
     groupId,
     artifactId,
@@ -13,16 +14,17 @@ const upload = () => {
     password,
   } = config;
   const MAVEN_COMMAND_URL = `mvn deploy:deploy-file -DgroupId=${groupId} -DartifactId=${artifactId} -Dversion=${version} -Dpackaging=aar -Dfile=${filePath} -Durl=${nexusUrl} -DrepositoryId=${repositoryId} -DuserName=${userName} -Dpassword=${password}`;
-  const platform = 'android';
 
   if (platform.toLowerCase() === 'android') {
     exeq(MAVEN_COMMAND_URL)
       .then(() => {
-        console.log('上传成功');
+        console.log(chalk.black.bgGreen('上传成功'));
         return Promise.resolve(true);
       })
       .catch(err => {
-        console.log(`上传失败 ${err}`);
+        console.log(chalk.black.bgGreen(`上传失败, error code is: ${JSON.stringify(err.code)}`));
+        console.log(chalk.yellow('请确保升级了 oriente.config.json.version 字段'));
+
         return Promise.reject(err);
       })
   } else {
