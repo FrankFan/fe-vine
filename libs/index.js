d616c8b3032a27143a4e15816380cce4e4fe0fa7
Khan Fan <khan.fan@oriente.com>
2018-07-16 12:29:48 +0800

    add ios upload zip

--- libs/index.js
+++ libs/index.js
@@ -12,6 +12,7 @@ const upload = (platform = 'android') => {
     repositoryId,
     userName,
     password,
+    feArchivePath,
   } = config;
   const MAVEN_COMMAND_URL = `mvn deploy:deploy-file -DgroupId=${groupId} -DartifactId=${artifactId} -Dversion=${version} -Dpackaging=aar -Dfile=${filePath} -Durl=${nexusUrl} -DrepositoryId=${repositoryId} -DuserName=${userName} -Dpassword=${password}`;
 
@@ -27,8 +28,21 @@ const upload = (platform = 'android') => {
 
         return Promise.reject(err);
       })
-  } else {
+  } else if (platform.toLowerCase() === 'ios') {
+    const POD_UPLOAD_URL = `orientepodspecpush --specRepo=OrienteSpecs --workspace=${feArchivePath} --noPackage --lint=" --allow-warnings --sources='ssh://git-codecommit.ap-southeast-1.amazonaws.com/v1/repos/ios-OrienteSpecs,https://github.com/CocoaPods/Specs.git'" --push=" --allow-warnings --sources='ssh://git-codecommit.ap-southeast-1.amazonaws.com/v1/repos/ios-OrienteSpecs,https://github.com/CocoaPods/Specs.git'"`;
     // iOS
+    console.log(POD_UPLOAD_URL);
+
+    exeq(POD_UPLOAD_URL)
+      .then(() => {
+        console.log(chalk.black.bgGreen('上传成功'));
+        return Promise.resolve(true);
+      })
+      .catch(err => {
+        console.log(chalk.black.bgGreen(`上传失败, error code is: ${JSON.stringify(err.code)}`));
+        return Promise.reject(err);
+      })
+      console.log(chalk.black.bgGreen('结束...'));
   }
 }
 
