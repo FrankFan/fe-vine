a3d91c4b995b7f3329b1fd84e9fa204a000c3f78
Khan Fan <khan.fan@oriente.com>
2018-07-18 18:44:02 +0800

    update

--- libs/index.js
+++ libs/index.js
@@ -21,7 +21,6 @@ const upload = (platform = 'android') => {
   } = config;
   artifactId = `${artifactId}-${mode}`;
   const MAVEN_COMMAND = `mvn deploy:deploy-file -DgroupId=${groupId} -DartifactId=${artifactId} -Dversion=${version} -Dpackaging=aar -Dfile=${arrFilePath} -Durl=${nexusUrl} -DrepositoryId=${repositoryId} -DuserName=${userName} -Dpassword=${password}`;
-  console.log(MAVEN_COMMAND);
   if (platform.toLowerCase() === 'android') {
     exeq(MAVEN_COMMAND)
       .then(() => {
@@ -35,16 +34,11 @@ const upload = (platform = 'android') => {
         return Promise.reject(err);
       })
   } else if (platform.toLowerCase() === 'ios') {
-    const zipFileName = zipFilePath.split('/').reverse()[0];
-    const zipName = zipFileName.split('.')[0];
-    const zipExtension = zipFileName.split('.')[1];
     const CP_ZIP_COMMAND = `cp ${zipFilePath} ${feArchiveZipPath}`;
-    const RENAME_ZIP_COMMAND = `mv ${feArchiveZipPath}/${zipFileName} ${feArchiveZipPath}/${zipName}-${version}.${zipExtension}`;
     const POD_UPLOAD_COMMAND = `orientepodspecpush --tag=${version} --specRepo=OrienteSpecs --workspace=${feArchiveRootPath} --noPackage --lint=" --allow-warnings --sources='ssh://git-codecommit.ap-southeast-1.amazonaws.com/v1/repos/ios-OrienteSpecs,https://github.com/CocoaPods/Specs.git'" --push=" --allow-warnings --sources='ssh://git-codecommit.ap-southeast-1.amazonaws.com/v1/repos/ios-OrienteSpecs,https://github.com/CocoaPods/Specs.git'"`;
 
     exeq(
       CP_ZIP_COMMAND,
-      RENAME_ZIP_COMMAND,
       `cd ${feArchiveRootPath}`,
       `git add .`,
       `git commit -m 'add zip file'`,
